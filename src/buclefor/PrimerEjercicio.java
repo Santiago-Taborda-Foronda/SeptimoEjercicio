02b296cd9b5bafb50538c1d18040d46e6d4bfbd2
Santiago <tabordasantiago350@gmail.com>
2024-07-29 01:26:15 -0500

    Septimo ejercicio

---
+++ src/buclefor/PrimerEjercicio.java
@@ -0,0 +1,13 @@
+package buclefor;
+
+public class PrimerEjercicio {
+	
+	    public static void main(String[] args) {
+	       
+	        for (int i = 1; i <= 5; i++) {
+	            System.out.println(i);
+	        }
+	    }
+	
+
+}
---
+++ src/buclefor/SegundoEjercicio.java
@@ -0,0 +1,18 @@
+package buclefor;
+
+public class SegundoEjercicio {
+	
+	    public static void main(String[] args) {
+	        int suma = 0; 
+
+	       
+	        for (int i = 1; i <= 10; i++) {
+	            suma += i;
+	        }
+
+	       
+	        System.out.println("La suma de los números del 1 al 10 es: " + suma);
+	    }
+	
+
+}
